410855b1be6ba056fbbe4d6d55f59fc12b062ca8
Jay7911 <jaehyun482@gmail.com>
2023-07-21 11:23:26 +0900

    오타 수정

--- misProject/src/main/java/com/spring/persistence/ProductDAO.java
+++ misProject/src/main/java/com/spring/persistence/ProductDAO.java
@@ -19,5 +19,5 @@ public interface ProductDAO {
 	// 페이징, 검색 기능 게시물 수 가져오는 기능
 	public int listSearchCount(SearchCriteria cri) throws Exception;
 	
-	public void updateViewCount(int bno) throws Exception;
+	public void updateViewCount(int pno) throws Exception;
 }
